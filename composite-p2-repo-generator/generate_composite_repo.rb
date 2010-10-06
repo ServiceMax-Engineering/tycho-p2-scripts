f0d34dfaea69b8d431544566c6f32a028863c619
hmalphettes <hmalphettes@gmail.com>
2010-10-06 15:11:41 -0700

    add the license

--- composite-p2-repo-generator/generate_composite_repo.rb
+++ composite-p2-repo-generator/generate_composite_repo.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
-# CL-159
+# ========================================================================
+# Copyright (c) 2006-2010 Intalio Inc
+# ------------------------------------------------------------------------
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# and Apache License v2.0 which accompanies this distribution.
+# The Eclipse Public License is available at 
+# http://www.eclipse.org/legal/epl-v10.html
+# The Apache License v2.0 is available at
+# http://www.opensource.org/licenses/apache2.0.php
+# You may elect to redistribute this code under either of these licenses. 
+# ========================================================================
+# Author hmalphettes
 #
 # This script generates a p2 composite repository:
 # http://wiki.eclipse.org/Equinox/p2/Composite_Repositories_(new)
