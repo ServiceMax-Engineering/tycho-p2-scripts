dfb6b2b7458df9e77f34b5b12e7aa3e9a13ad05f
hmalphettes <hmalphettes@gmail.com>
2010-10-08 16:54:11 -0700

    workaround BUILD-524

--- osgi-features-to-debian-package/buildr-generate-deb-from-features.rb
+++ osgi-features-to-debian-package/buildr-generate-deb-from-features.rb
@@ -37,7 +37,7 @@ def eval_file_task(file, contents = nil)
   end
 end
 
-def eval_file(file, contents = nil, label = nil)
+def eval_file(file, contents = nil, label = nil, version = nil)
   buffer = _("target/#{File.basename(file)}") 
   rm buffer if File.exists? buffer
   File.open(buffer, "w") do |writer|
@@ -77,6 +77,21 @@ def load_properties(properties_filename)
   properties
 end
 
+# Parse the MANIFEST.MF entry of a ZIP (or JAR) file and return a new Manifest.
+# we can't use the from_zip method in buildr-1.4.2 because it crashes
+#when it reads the jar produced by maven.
+# see BUILDR-???
+def read_manifest_from_jar(jar_file)
+  Zip::ZipInputStream::open(jar_file.to_s) { |io|
+    while (entry = io.get_next_entry)
+      if entry.name == 'META-INF/MANIFEST.MF'
+        return ::Buildr::Packaging::Java::Manifest.parse io.read rescue ::Buildr::Packaging::Java::Manifest.new
+      end
+    end
+  }
+  return ::Buildr::Packaging::Java::Manifest.new
+end
+
 def read_built_feature_version(control_file)
   feature_project_folder = File.dirname(control_file)
   target_folder = File.join(feature_project_folder, "target")
@@ -85,15 +100,26 @@ def read_built_feature_version(control_file)
     xmlfile = File.new File.join(target_folder, "feature.xml")
     xmlfeat = Document.new(xmlfile)
     return xmlfeat.root.attributes["version"]
-  else
-    #look for the product filename
-    #at the moment we support a single product file.
-    Dir.glob(File.join(target_folder, "*.product")).each do|product_file|
-      xmlfile = File.new product_file
-      xmlprod = Document.new(xmlfile)
-      return xmlprod.root.attributes["version"]
+  end
+  #look for the product filename
+  #at the moment we support a single product file.
+  Dir.glob(File.join(target_folder, "*.product")).each do|product_file|
+    xmlfile = File.new product_file
+    xmlprod = Document.new(xmlfile)
+    return xmlprod.root.attributes["version"]
+  end
+  
+  #look for the MANIFEST.MF of the built bundle
+  Dir.glob(File.join(target_folder, "*.jar")).each do|jar_file|
+    #make sure it is not the source bundle:
+    if (jar_file =~ /-source/).nil?
+      read_m = read_manifest_from_jar(_(jar_file).to_s).main
+      return read_m["Bundle-Version"]
     end
   end
+
+  raise "Could not find a built feature.xml, or a built product file or a built bundle in #{target_folder}"
+  
 end
 
 # Reads the built target/feature.xml file
@@ -105,6 +131,7 @@ def build_deb(control_file,deb)
   featfile=File.join(target_folder, "feature.xml")
   id="unknown"
   if File.exist? featfile
+    puts "Exposing #{featfile} as #{control_file}"
     xmlfile = File.new featfile
     xmlfeat = Document.new(xmlfile)
     version=xmlfeat.root.attributes["version"]
@@ -116,22 +143,40 @@ def build_deb(control_file,deb)
       label=feature_properties[label]
     end
   else
-    puts "No feature.xml looking for products"
     #look for the product filename
     #at the moment we support a single product file.
     Dir.glob(File.join(target_folder, "*.product")).each do|product_file|
-    puts "prod #{product_file}"
+      puts "Exposing #{product_file} as #{control_file}"
       xmlfile = File.new product_file
       xmlprod = Document.new(xmlfile)
       version=xmlprod.root.attributes["version"]
       id=xmlprod.root.attributes["uid"]
       label=xmlprod.root.attributes["name"]
     end
+    if id == "unknown"
+      #look for the MANIFEST.MF of the built bundle
+      Dir.glob(File.join(target_folder, "*.jar")).each do|jar_file|
+        #make sure it is not the source bundle:
+        if (jar_file =~ /-source/).nil?
+          puts "Exposing #{jar_file} as #{control_file}"
+          read_m = read_manifest_from_jar(_(jar_file).to_s).main
+          version=read_m["Bundle-Version"]
+          id=read_m["Bundle-Version"]
+          label=read_m["Bundle-Description"]
+          if label.nil?
+            label=read_m["Bundle-Name"]
+            if label.nil?
+              label=read_m["Bundle-SymbolicName"]
+            end
+          end
+        end
+      end
+    end
   end
 
-  puts "version=#{version}"
-  puts "id=#{id}"
-  puts "label=#{label}"
+#  puts "version=#{version}"
+#  puts "id=#{id}"
+#  puts "label=#{label}"
 
   control_content=nil
   if controle_file_empty?(control_file)
@@ -148,7 +193,7 @@ Description: #{label}
 CONTROL
   end
   
-  built_control=eval_file(control_file, control_content, label)
+  built_control=eval_file(control_file, control_content, label, version)
 
   iu_file=File.join(feature_project_folder,File.basename(control_file).chomp(".control")+".ius")
   ius_content = <<-IUS
@@ -157,7 +202,7 @@ CONTROL
 # Note: the version is controlled by the cloud-all repository; not by the debian package
 #{id}
 IUS
-  built_ius=eval_file(iu_file, ius_content, label)
+  built_ius=eval_file(iu_file, ius_content, label, version)
  
   deb.version=version
   deb.control=built_control.to_s
