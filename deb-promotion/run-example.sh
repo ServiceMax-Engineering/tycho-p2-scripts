e201fc46f30a9e6eb9f34199c33b8a8421df4a33
hmalphettes <hmalphettes@gmail.com>
2011-01-07 17:29:42 +0800

    first attempt at mirroring cloud-all and aggregating it

--- deb-promotion/run-example.sh
+++ deb-promotion/run-example.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-site_url=$HOME/p2repo/com/intalio/cloud/all/3.1.1.345
-destination=$HOME/tmp_p2_try
+export site_url=$HOME/p2repo/com/intalio/cloud/all/3.1.1.345
+export destination=$HOME/tmp_p2_try
 mkdir -p $HOME/tmp_p2_try
-P2_DIRECTOR_HOME=$HOME/tools/p2director-20101010
-export site_url
-export destination
+export P2_DIRECTOR_HOME=$HOME/tools/p2director-20101010
 ./mirror.sh
